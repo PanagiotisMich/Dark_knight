33fe39b3bacfab65ce2d197e5ce4967a643b446f
PanagiotisMich <72735754+PanagiotisMich@users.noreply.github.com>
2022-10-03 17:50:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-Username=input('What is your username:')
-Password=input('What is your password:')
-password_lenth=len(Password)
-hidden_password='*'*password_lenth
-print(f'{Username} your Password {hidden_password} is {password_lenth}')
+print('a man in a clown mask holding a smoking silenced pistol ejects a shell casing.This is Dopey. He turns to a second man ,Happy also,also in a clown mask who steps forward with a CABLE LAUNCHER,aims at the lower roof across the street and fires a cable across.Dopey secures the line to an I-beam line -CAMP on - sends a kit bag out and then steps out of the window  ')
+print('\n')
+print('A man in the corner back to us ,holding a clown mask .An SUV pulls up.The man gets in ,put in his mask inside the car two other men wear masks')
+print('\n')
+print('The GRUMPY gear is  ')
+GRUMPY = {
+  "pistol": "ALFA Defender",
+  "manufacturer": "ALFA-PROJ",
+  "Cartridge": '9×19mm Parabellum .40 S&W .45 ACP'
+}
+print(GRUMPY)
+print('\n')
+print('The HAPPY gear is ')
+HAPPY = {
+  "pistol": "AMT AutoMag III",
+  "manufacturer": "Arcadia Machine & Tool",
+  "Cartridge": '30 Carbine 9mm Winchester Magnum'
+}
+print(HAPPY)
+print('\n')
+print('The CHUCKLES gear is ')
+CHUCKLES = {
+  "pistol": "AMT AutoMag II",
+  "manufacturer": "Arcadia Machine & Too",
+  "Cartridge": '.22 Winchester Magnum Rimfire'
+}
+print(CHUCKLES)
+print('\n')
+print('The DOPEY gear is ')
+DOPEY = {
+  "pistol": "AMT AutoMag IV",
+  "manufacturer": "Arcadia Machine & Tool",
+  "Cartridge": '.45 Winchester Magnum'
+}
+print(DOPEY)
+print('\n')
+print('GRUMPY:' +'Three of a kind.Let 1 do this ')
+print('CHUCKLES:' +'That\s it? Three guys?')
+print('GRUMPY:' +'There\s two on the roof ,every guy is an extra share five shares is plenty')
+
+
